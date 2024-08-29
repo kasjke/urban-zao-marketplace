00c894349c17023c66386751d711ba0d58972649
Kateryna Shvets <32341036+Enigma2017@users.noreply.github.com>
2024-08-30 02:32:13 +0300

    added logo and header additional lines

---
+++ Frontend/src/shared/header-menu/navigation-panel.tsx
@@ -0,0 +1,15 @@
+export const NavigationPanel = () => {
+    return (
+        <>
+            <div className="navigation-panel__promoline">
+                <p>New Items on sale | UP TO %50 OFF</p>
+            </div>
+            <div className="navigation-panel__topline"></div>
+            <div className="navigation-panel">
+                <div className="header-logo">
+                    <a href="/" className="header-logo__link">Urban Zen</a>
+                </div>
+            </div>
+        </>
+    )
+}
--- Frontend/src/shared/header.tsx
+++ Frontend/src/shared/header.tsx
@@ -1,13 +1,11 @@
 import { NavigationMenu } from "./header-menu/navigation-menu"
-import { NavigationSearch } from "./header-menu/navigation-search"
-import { NavigationUser } from "./header-menu/navigation-user"
+import { NavigationPanel } from "./header-menu/navigation-panel"
 
 
 export const Header = () => {
     return (
         <div>
-            <NavigationSearch />
-            <NavigationUser />
+            <NavigationPanel />
             <NavigationMenu />
         </div>
     )
