a26975770afb962b2cdab67286cd1259f65adcc7
Kateryna Shvets <32341036+Enigma2017@users.noreply.github.com>
2024-08-30 14:23:05 +0300

    added search panel

--- Frontend/src/shared/header-menu/navigation-panel.tsx
+++ Frontend/src/shared/header-menu/navigation-panel.tsx
@@ -1,15 +1,17 @@
-export const NavigationPanel = () => {
-    return (
-        <>
-            <div className="navigation-panel__promoline">
-                <p>New Items on sale | UP TO %50 OFF</p>
-            </div>
-            <div className="navigation-panel__topline"></div>
-            <div className="navigation-panel">
-                <div className="header-logo">
-                    <a href="/" className="header-logo__link">Urban Zen</a>
-                </div>
-            </div>
-        </>
-    )
-}
+import React from 'react';
+
+export const NavigationPanel: React.FC = () => {
+  return (
+    <>
+      <div className="navigation-panel__promoline">
+        <p>New Items on sale | UP TO %50 OFF</p>
+      </div>
+      <div className="navigation-panel__topline"></div>
+      <div className="navigation-panel">
+        <div className="header-logo">
+          <a href="/" className="header-logo__link">Urban Zen</a>
+        </div>
+      </div>
+    </>
+  );
+};
--- Frontend/src/shared/header-menu/navigation-search.tsx
+++ Frontend/src/shared/header-menu/navigation-search.tsx
@@ -1,7 +1,18 @@
+import { SearchIcon } from "../icons/search";
+import { OpenSearchIcon } from "../icons/open-search";
+
 export const NavigationSearch = () => {
     return (
         <div>
-            Navigation Search
+            <form>
+                <button>
+                    <OpenSearchIcon />
+                </button>
+                <input type="text" placeholder="Search apparel, figures, and more" />
+                <button type="submit">
+                    <SearchIcon />
+                </button>
+            </form>
         </div>
     )
 }
